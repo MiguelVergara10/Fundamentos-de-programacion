4bf81a87d14081feee74367b37c046985909966c
MiguelVergara10 <vergararodriguezjesusmiguel@gmail.com>
2025-05-07 15:44:59 -0700

    Add files via upload

---
+++ Ejercicio 3.py
@@ -0,0 +1,10 @@
+import math
+
+# Pedir al usuario el radio
+radio = float(input("Ingresa el radio de la circunferencia: "))
+
+# Calcular la longitud (perímetro)
+Perimetro = 2 * math.pi * radio
+
+# Mostrar el resultado
+print(f"El perimetro de la circunferencia es: {Perimetro:.2f}")
